2583853ba4401311153db41c789f37a350bb7c84
dantekasev <47272068+dantekasev@users.noreply.github.com>
2023-01-26 17:11:45 +0700

    update_application_features

--- CRUD_textbased/app_cruds.py
+++ CRUD_textbased/app_cruds.py
@@ -45,11 +45,37 @@ def update_data(db):
  db.commit()
  print("{} data berhasil diubah".format(cursor.rowcount))
 
+def delete_data(db):
+  cursor = db.cursor()
+  show_data(db)
+  customer_id = input("pilih id cusstomer > ")
+  sql = "delete from customers where customer_id=%s"
+  val = (customer_id,)
+  cursor.execute(sql, val)
+  db.commit()
+  print("{} data berhasil dihapus".format(cursor.rowcount))
+
+def search_data(db):
+  cursor = db.cursor()
+  keyword = input("kata kunci: ")
+  sql = "select * from customers where name like %s or address like %s"
+  val = ("%{}%".format(keyword), "%{}%".format(keyword),)
+  cursor.execute(sql, val)
+  results = cursor.fetchall()
+
+  if cursor.rowcount < 0:
+    print("Tidak ada data")
+  else:
+    for data in results:
+      print(data)
+
 def show_menu(db):
  print("==APLIKASI CRUD DATABASE PYTHON==")
  print("1. Insert Data")
  print("2. Tampilkan Data")
  print("3. Update Data")
+ print("4. Delete Data")
+ print("5. Search Data")
  print("0. Keluar")
  menu = input("Pilih menu> ")
 
@@ -63,6 +89,10 @@ def show_menu(db):
  elif menu == "3":
   update_data(db)
  elif menu == "4":
+  delete_data(db)
+ elif menu == "5":
+  search_data(db)
+ elif menu == "0":
   exit()
  else:
   print("menu salah!")
